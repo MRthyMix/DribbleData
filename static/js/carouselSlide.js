d54e61bbd4b5b2db447fe5886309c8f25ab7c154
David Cruz <davidthylife@gmail.com>
2026-04-08 20:45:33 -0400

    click issue

--- static/js/carouselSlide.js
+++ static/js/carouselSlide.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
     var $slider = $('.carousel-slide');
+    var mouseDownX = 0;
+    var mouseDownY = 0;
+    window.sliderDragged = false;
+
+    $slider.on('mousedown touchstart', function(e) {
+        var point = e.touches ? e.touches[0] : e;
+        mouseDownX = point.clientX;
+        mouseDownY = point.clientY;
+        window.sliderDragged = false;
+    });
+
+    $slider.on('mouseup touchend', function(e) {
+        var point = e.changedTouches ? e.changedTouches[0] : e;
+        var dx = Math.abs(point.clientX - mouseDownX);
+        var dy = Math.abs(point.clientY - mouseDownY);
+        if (dx > 5 || dy > 5) {
+            window.sliderDragged = true;
+            setTimeout(function() { window.sliderDragged = false; }, 100);
+        }
+    });
 
     $slider.slick({
       slidesToShow: 5,
