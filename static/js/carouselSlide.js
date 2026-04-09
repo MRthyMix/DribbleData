f4de5784e8e79819cbecb3ed9c58cb6b299662f7
David Cruz <davidthylife@gmail.com>
2026-04-08 20:38:47 -0400

    update33

--- static/js/carouselSlide.js
+++ static/js/carouselSlide.js
@@ -5,27 +5,12 @@ $(document).ready(function(){
       slidesToShow: 5,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 1,
-      speed: 10000,
-      cssEase: 'ease-in-out',
+      autoplaySpeed: 0,
+      speed: 3000,
+      cssEase: 'linear',
       arrows: true,
       infinite: true,
-      pauseOnHover: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
     });
-
-    function styleSlides(slickObj) {
-      const start = slickObj.currentSlide;
-      slickObj.$slides.css({ opacity: 0.2, transform: 'scale(1)' });
-
-      [1,2,3].forEach(offset => {
-        const idx = start + offset;
-        $(slickObj.$slides.get(idx)).css({
-          opacity: 1,
-          transform: 'scale(1.05)'
-        });
-      });
-    }
-
-    $slider.on('afterChange', (e, slick) => styleSlides(slick));
-    styleSlides($slider.slick('getSlick'));
   });
